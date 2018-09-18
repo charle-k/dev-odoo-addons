e88b6d488a49ec3618025e353642e039605eadad
charle-k <katuricharles@gmail.com>
2018-09-18 15:27:47 +0200

    added library module

---
+++ my_module/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Library Books",
+    'summary': "Manage your books",
+    'depends': ['base'],
+    'data': ['security/groups.xml',
+             'views/library_book.xml',
+             'security/ir.model.access.csv',
+ ],
+}
